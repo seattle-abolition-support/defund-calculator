9151df35cf099c0b13edb3c3cd1b03275b5424a6
unknown <drewbat@outlook.com>
2021-07-19 23:38:56 -0700

    Adding Resources, About, and Victory pages

--- src/App.js
+++ src/App.js
@@ -8,34 +8,7 @@ import Data from "./data.json";
 import LittleCheckMark from "./images/interface stuff/little-check-mark.svg"
 import LittleShield from "./images/interface stuff/little-shield.svg"
 
-// import Batton from "./images/cop stuff/batton.svg"
-// import BigBadge from "./images/cop stuff/big-badge.svg"
-// import CopCar from "./images/cop stuff/cop-car.svg"
-// import GasMask from "./images/cop stuff/gas-mask.svg"
-// import Gun from "./images/cop stuff/gun.svg"
-// import Handcuffs from "./images/cop stuff/handcuffs.svg"
-// import Helicopter from "./images/cop stuff/helicopter.svg"
-// import Motorcycle from "./images/cop stuff/motorcycle.svg"
-// import Vest from "./images/cop stuff/vest.svg"
-// import WalkieTalkie from "./images/cop stuff/walkie-talkie.svg"
 
-// import Carrot from "./images/city stuff/carrot.svg"
-// import Crayon from "./images/city stuff/crayon.svg"
-// import MedicalWorker from "./images/city stuff/medical-worker.svg"
-// import PrideFlag from "./images/city stuff/pride-flag.svg"
-// import Rainbow from "./images/city stuff/rainbow.svg"
-// import RedApple from "./images/city stuff/red-apple.svg"
-// import SunWithFace from "./images/city stuff/sun-with-face.svg"
-// import TownHouses from "./images/city stuff/town-houses.svg"
-// import Tulip from "./images/city stuff/tulip.svg"
-// import WhiteCloud from "./images/city stuff/white-cloud.svg"
-
-
-// import Skyline0 from "./images/city stuff/skyline0.svg"
-// import Skyline1 from "./images/city stuff/skyline1.svg"
-// import Skyline2 from "./images/city stuff/skyline2.svg"
-// import Skyline3 from "./images/city stuff/skyline3.svg"
-// import Skyline4 from "./images/city stuff/skyline4.svg"
 
 export default class App extends React.Component {
   constructor(props) {
@@ -46,6 +19,9 @@ export default class App extends React.Component {
       statusMessage: Data.InitStatusMessage,
       categoriesUnlocked: false,
       showMe: false,
+      showResources: false,
+      showAbout: false,
+      showVictory: false,
       gameDefundCount: 0,
       gameFundCount: 0,
       levelUp: 0
@@ -101,6 +77,7 @@ export default class App extends React.Component {
         message = d.UI.LevelUpText;
       }
     }
+
     
     // Set state. Update data, message, categoriesUnlocked, levelUp
     this.setState((state, props) => ({
@@ -122,10 +99,12 @@ export default class App extends React.Component {
     //console.log("CurrentOfficerCount: " + d.CurrentOfficerCount);
 
     // If zero officers left, victory!
+    let showVictory = this.state.showVictory;
     if(d.CurrentOfficerCount <= 0)
     {
       // TBD
       // Victory dialog
+      showVictory = true;
       d.CurrentOfficerCount = 0;
       // VICTORY!!!
     }
@@ -143,7 +122,8 @@ export default class App extends React.Component {
     this.setState((state, props) => ({
       data : d,
       gameDefundCount : gameDefundCount,
-      statusMessage : d.UI.CopPhrases[Math.floor(Math.random() * d.UI.CopPhrases.length)]
+      statusMessage : d.UI.CopPhrases[Math.floor(Math.random() * d.UI.CopPhrases.length)],
+      showVictory: showVictory
     }), () => this.recalculateBudgets());
 
 
@@ -259,6 +239,24 @@ export default class App extends React.Component {
     }
   }
 
+  toggleResources = () => {
+    this.setState((state, props) => ({
+      showResources : (! state.showResources)
+    }));
+  }
+
+  toggleAbout = () => {
+    this.setState((state, props) => ({
+      showAbout : (! state.showAbout)
+    }));
+  }
+
+  toggleVictory = () => {
+    this.setState((state, props) => ({
+      showVictory : (! state.showVictory)
+    }));
+  }
+
   formatDollars = (s) =>  {
     return "$" + s.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
@@ -318,6 +316,15 @@ export default class App extends React.Component {
       {this.state.showMe ? 
           <ShowMePage data={this.state.data} formatDollars={this.formatDollars} toggleShowMe={this.toggleShowMe}/> : null
       }
+      {this.state.showResources ? 
+          <ResourcesPage data={this.state.data} toggleResources={this.toggleResources}/> : null
+      }
+      {this.state.showAbout ? 
+          <AboutPage data={this.state.data} toggleAbout={this.toggleAbout}/> : null
+      }
+      {this.state.showVictory ? 
+          <VictoryPage data={this.state.data} toggleVictory={this.toggleVictory}/> : null
+      }
       <div className="Main" style={(isMobile) ? {} : {width: "500px"}}>
         
         <div className="MainTitle">{this.state.data.UI.MainTitle}</div>
@@ -373,10 +380,10 @@ export default class App extends React.Component {
           <div className={showMeButtonClass} onClick={() => this.toggleShowMe()}>
             {this.state.data.UI.ShowMeText}
           </div>
-          <div className="ResourcesButton">
+          <div className="ResourcesButton" onClick={() => this.toggleResources()}>
             {this.state.data.UI.ResourcesText}
           </div>
-          <div className="AboutButton">
+          <div className="AboutButton" onClick={() => this.toggleAbout()}>
             {this.state.data.UI.AboutText}
           </div>
         </div>
@@ -386,162 +393,8 @@ export default class App extends React.Component {
   }
 }
 
-// class Game extends React.Component {
-  
-//   constructor(props) {
-//     super(props);
-
-//     // let copImages = [Batton, BigBadge, CopCar, GasMask, Gun, Handcuffs, Helicopter, Motorcycle, Vest, WalkieTalkie];
-//     // let baseScale = [.5, .7, 1, 1.0, 1, .7, 1.1, 1.1, .5, .3];
-//     // let baseRotation = [180, 30, 30, 90, 180, 180, 45, 0, 0, 90  ];
-
-//     let copImages = [BigBadge, CopCar, GasMask, Gun, Handcuffs, Helicopter, Motorcycle, WalkieTalkie];
-//     let baseScale = [.7, 1, 1.0, 1, .7, 1.1, 1.1, .3];
-//     let baseRotation = [30, 30, 90, 180, 180, 45, 0, 90  ];
-
-//     let cityImages = [Carrot, Crayon, MedicalWorker, PrideFlag, Rainbow, RedApple, TownHouses, Tulip]
-    
-
-//     let skylineImages = [Skyline0, Skyline1, Skyline2, Skyline3, Skyline4];
-
-//     let spriteLayers = 5;
-//     let baseLayerCount = 9;
-//     let spriteData = [];
-//     let cityData = [];
-//     let skylineData = [];
-//     for (var i = 0; i < spriteLayers; i++) {
-
-//       var layerScale = 1 - ((i / spriteLayers) * .5);
-//       var layerCount = Math.floor(baseLayerCount / layerScale);
-//       for(var j = 0; j < layerCount; j++) {
-//         let imgIdx = Math.floor(Math.random() * copImages.length);
-//         let x = -10 + 120 * (j / layerCount);
-//         let y = 70 - 80 * (i / spriteLayers);
-//         let z = Math.floor(100 * ((spriteLayers - i) + Math.random()));
-//         let s = layerScale * 30 * baseScale[imgIdx];
-
-//         spriteData.push(
-//           {
-//             x: x,
-//             y: y,
-//             img: copImages[imgIdx],
-//             scale: s,
-//             rotation: 0,//Math.floor((Math.random() * baseRotation[imgIdx]) - (baseRotation[imgIdx] * .5))
-//             zindex: z
-//           }
-//         )
-
-//         let slx = 0;
-//         let sly = 70 - (60 * (i / spriteLayers));
-//         let slz = Math.floor(100 * (spriteLayers - i));
-//         let simage = skylineImages[i % skylineImages.length]
-
-//         skylineData.push(
-//           {
-//             img: simage,
-//             x: slx,
-//             y: sly,
-//             zindex: slz,
-//             scale: 100
-//           }
-//         )
-
-//       }
-//     }
-
-//     // reverse Z
-//     spriteData.sort((a, b) => {
-//       return a.zindex - b.zindex;
-//     });
-
-//     this.state = { spriteData: spriteData, skylineData: skylineData, spriteLayers: spriteLayers, cityData: cityData, cityImages: cityImages}
-//   }
-
-  
-
-//   render() {    
-    
-
-//     return (
-//       <div style={{ position: "relative", width: "100%", height: "100%", overflow: "hidden"}}>
-//         {this.state.spriteData.map((spriteData, index) => (
-//           <img src={spriteData.img}  style={{ position: "absolute", left: (spriteData.x ).toString() + "%",  top: (spriteData.y).toString() + "%",  width: spriteData.scale.toString() + "vmin", height: "auto", zIndex: (spriteData.zindex).toString(), transform: "rotate(" + spriteData.rotation + "deg)"}}  alt="" key={"copsprite_" + index}/>
-//        ))}
-//        {this.state.skylineData.map((skylineData, index) => (
-//           <img src={skylineData.img}  style={{ position: "absolute", left: (skylineData.x ).toString() + "%",  top: (skylineData.y).toString() + "%",  width: skylineData.scale.toString() + "%", height: "auto", zIndex: (skylineData.zindex).toString()}}  alt="" key={"skyline_" + index}/>
-//        ))}
-//        {this.state.cityData.map((cityData, index) => (
-//           <img src={cityData.img}  style={{ position: "absolute", left: (cityData.x ).toString() + "%",  top: (cityData.y).toString() + "%",  width: cityData.scale.toString() + "%", height: "auto", zIndex: (cityData.zindex).toString()}}  alt="" key={"citysprite_" + index}/>
-//        ))}
-//       </div>
-//     );
-//   }
-
-
-//   componentDidUpdate(prevProps, prevState) {
-//     console.log("game componentDidUpdate " + this.props.gameDefundCount);
-//     console.log(this.props);
-//     if(prevProps.gameDefundCount === 0 && this.props.gameDefundCount !== 0) {
-      
-
-
-//       let spriteData = this.state.spriteData;
-//       for(var i = 0; i < this.props.gameDefundCount; i++) {
-//         spriteData.pop();
-//       }
-//       this.setState((state, props) => ({
-//         spriteData : spriteData
-//       }));
-//     }
-//     if(prevProps.gameFundCount == 0 && this.props.gameFundCount != 0)
-//     {
-//       let cityData = this.state.cityData;
-//       if(this.props.gameFundCount > 0) {
-//         let newCityItem = this.getNewCityItem();
-//         console.log("newCityItem " + newCityItem);
-//         cityData.push(newCityItem);
-//       }else{
-//         cityData.pop();
-//       }
-//       this.setState((state, props) => ({
-//         cityData : cityData
-//       }));
-
-//     }
-//   }
-
-//   getNewCityItem = () => {
-
-
-//     let img = this.state.cityImages[Math.floor(Math.random() * this.state.cityImages.length)];
-
-    
-//     let zHack = this.state.spriteLayers - Math.floor((this.state.spriteLayers * (this.state.cityData.length / 50)) % this.state.spriteLayers);
-
-//     let x = Math.floor(Math.random() * 100);
-//     let y = Math.floor(80 - (1 - (zHack / this.state.spriteLayers)) * 60);
-    
-//     let z = Math.floor(100 * (zHack + Math.random()));
-
-//     return {
-//         img: img,
-//         x: x,
-//         y: y,
-//         zindex: z,
-//         scale: 10
-//       };
-//   }
-// }
 
 class ShowMePage extends React.Component {
-  
-  // constructor(props) {
-  //   super(props);
-
-
-  //   this.state = { spriteData: spriteData}
-  // }
-
   
 
   render() {    
@@ -611,6 +464,80 @@ class ShowMePage extends React.Component {
           </div>
         ))}
         <div className="ReturnToDefundButton" onClick={() => this.props.toggleShowMe()}>{this.props.data.UI.ReturnToDefundText}</div>
+      </div>
+    );
+  }
+}
+
+
+class ResourcesPage extends React.Component {
+  
+
+  render() {    
+    const d = this.props.data;
+    
+
+
+    return (
+      <div className="ResourcesScreen" style={(isMobile) ? {} : {width: "500px", left: "50%", transform: "translateX(-50%)"}}>
+        <div className="ShowMeTitle">RESOURCES</div>
+        {d.Resources.map((resource, index) => (
+          <a href={resource.LinkUrl} style={{textDecoration: "none"}} target="_blank">
+          <div key={"resources_" + index} className="ResourceButton">
+              {resource.LinkText}        
+          </div> 
+          </a>
+        ))}
+        <div className="ReturnToDefundButton" onClick={() => this.props.toggleResources()}>{this.props.data.UI.ReturnToDefundText}</div>
+      </div>
+    );
+  }
+}
+
+class AboutPage extends React.Component {
+  
+
+  render() {    
+    const d = this.props.data;
+    
+
+
+    return (
+      <div className="AboutScreen" style={(isMobile) ? {} : {width: "500px", left: "50%", transform: "translateX(-50%)"}}>
+        <div className="ShowMeTitle">ABOUT</div>
+        <div>
+          <p>Some text here about how we arrived at different amounts for police budget / police officer defunding, and sources of information.</p>
+          <p>Information on how PB works</p>
+          <p>(info graphic and/or video?)</p>
+          <p>Credits: Developed by PB Creators, Seattle Abolition Support / Defend the Defund</p>
+          <p><a href="mailto: something">EMAIL</a></p>
+        </div>
+        <div className="ReturnToDefundButton" onClick={() => this.props.toggleAbout()}>{this.props.data.UI.ReturnToDefundText}</div>
+        
+      </div>
+    );
+  }
+}
+
+class VictoryPage extends React.Component {
+  
+
+  render() {    
+    const d = this.props.data;
+    
+
+
+    return (
+      <div className="VictoryScreen" style={(isMobile) ? {} : {width: "500px", left: "50%", transform: "translateX(-50%)"}}>
+        <div className="ShowMeTitle">VICTORY!</div>
+        <div>
+          <p>CONGRATULATIONS!</p> 
+          <p>YOU HAVE ABOLISHED THE SEATTLE POLICE DEPARTMENT!</p> 
+          <p>JOIN THE EFFORT AT:</p>
+          <p><a href="bit.ly/DefendTheDefund">bit.ly/DefendTheDefund</a></p>
+        </div>
+        <div className="VictoryReturnButton" onClick={() => this.props.toggleVictory()}>{this.props.data.UI.VictoryReturnText}</div>
+        
       </div>
     );
   }
